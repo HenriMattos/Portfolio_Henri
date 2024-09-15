1577c7753b21c441e4d09ccae7f1d7ecd0fda613
Henri Mattos <145288353+HenriMattos@users.noreply.github.com>
2024-09-15 17:17:00 -0400

    Add files via upload

---
+++ dot.js
@@ -0,0 +1,28 @@
+let slideIndex = 1;
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll(".carousel-slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    document.querySelector(".carousel-slides").style.transform = `translateX(${-100 * (slideIndex - 1)}%)`;
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex - 1].className += " active";
+}
+
+setInterval(function() { changeSlide(1); }, 7000);
+
+showSlides(slideIndex);
